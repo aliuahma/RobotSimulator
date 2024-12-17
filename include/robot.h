68a15139bf1f359c6721c8ec2a96bfaa238980e2
AliAhmad810 <hiimaliahmad@gmail.com>
2024-12-17 11:54:49 -0800

    initial code just to test

---
+++ include/robot.h
@@ -0,0 +1,8 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+class Robot {
+
+};
+
+#endif // ROBOT_H
---
+++ include/simulator.h
@@ -0,0 +1,10 @@
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include <SFML/Graphics.hpp>
+
+class Simulator {
+
+};
+
+#endif // SIMULATOR_H
